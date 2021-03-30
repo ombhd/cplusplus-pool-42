5d2c631cbd46105825c78736ecb8ab2f74062527
OMAR BOUYKOURNE <omarbpixel@gmail.com>
2021-03-30 19:14:37 +0100

    ex02: loading..

---
+++ D08/ex02/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouykou <obouykou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/30 19:02:38 by obouykou          #+#    #+#             */
+/*   Updated: 2021/03/30 19:02:57 by obouykou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mutantstack.hpp"
+
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
---
+++ D08/ex02/mutantstack.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mutantstack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obouykou <obouykou@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/30 19:03:05 by obouykou          #+#    #+#             */
+/*   Updated: 2021/03/30 19:05:59 by obouykou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANT_STACK_H
+#define MUTANT_STACK_H
+
+class MutantStack
+{
+private:
+	/* data */
+public:
+	MutantStack(/* args */);
+	~MutantStack();
+};
+
+
+#endif // !MUTANT_STACK_H
