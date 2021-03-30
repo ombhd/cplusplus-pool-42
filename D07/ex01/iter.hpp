510fe689987bb2efeb7b952b9491b3d8b584ab8b
OMAR BOUYKOURNE <omarbpixel@gmail.com>
2021-03-30 13:31:26 +0100

    ex01: fix const parameter

--- D07/ex01/iter.hpp
+++ D07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: obouykou <obouykou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/27 18:43:42 by obouykou          #+#    #+#             */
-/*   Updated: 2021/03/27 19:10:09 by obouykou         ###   ########.fr       */
+/*   Updated: 2021/03/30 13:29:04 by obouykou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <iostream>
 
 template <typename T>
-void	iter(T *arr, size_t &count, void (*func)(T &))
+void	iter(T *arr, const size_t &count, void (*func)(T &))
 {
 	for (size_t i = 0; i < count; i++)
 	{
