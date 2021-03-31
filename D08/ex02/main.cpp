2ebf96eaca5510ecadcb85447275ba1fca6d7708
OMAR BOUYKOURNE <omarbpixel@gmail.com>
2021-03-31 14:11:15 +0100

    ex02: done !

--- D08/ex02/main.cpp
+++ D08/ex02/main.cpp
@@ -6,34 +6,87 @@
 /*   By: obouykou <obouykou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 19:02:38 by obouykou          #+#    #+#             */
-/*   Updated: 2021/03/30 19:02:57 by obouykou         ###   ########.fr       */
+/*   Updated: 2021/03/31 14:10:22 by obouykou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
+#include <list>
 
 #include "mutantstack.hpp"
 
 int main()
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
-	{
-	std::cout << *it << std::endl;
-	++it;
+
+	{	
+        std::cout << "\n\033[33mUSING STACK\033[0m\n" << std::endl;
+		MutantStack<int> mstack;
+
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << "Top  ==> " << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << "Size ==> "  << mstack.size() << std::endl;
+		
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(13);
+		mstack.push(37);
+		
+		std::cout << "Top  ==> " << mstack.top() << std::endl;
+		std::cout << "Size ==> "  << mstack.size() << std::endl;
+		
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		std::cout << "Elements: [  ";
+		while (it != ite)
+		{
+			std::cout << *it << "  ";
+			++it;
+		}
+		std::cout << "]" << std::endl;
+		
+		std::stack<int> s(mstack);
 	}
-	std::stack<int> s(mstack);
+	
+	std::cout << "\n===================================================" << std::endl;
+	
+	{
+        std::cout << "\n\033[33mUSING LIST\033[0m\n" << std::endl;
+		std::list<int> mylist;
+
+		mylist.push_back(5);
+		mylist.push_back(17);
+		std::cout << "Top  ==> " << mylist.back() << std::endl;
+		mylist.pop_back();
+		std::cout << "Size ==> " << mylist.size() << std::endl;
+
+		mylist.push_back(3);
+		mylist.push_back(5);
+		mylist.push_back(13);
+		mylist.push_back(37);
+		
+		std::cout << "Top  ==> " << mylist.back() << std::endl;
+		std::cout << "Size ==> "  << mylist.size() << std::endl;
+		
+		std::list<int>::iterator it = mylist.begin();
+		std::list<int>::iterator ite = mylist.end();
+		++it;
+		--it;
+		std::cout << "Elements: [  ";
+		while (it != ite)
+		{
+			std::cout << *it << "  ";
+			++it;
+		}
+		std::cout << "]" << std::endl;
+		
+		std::list<int> s(mylist);
+	}
+
+	std::cout << std::endl;
+
 	return 0;
 }
--- D08/ex02/mutantstack.hpp
+++ D08/ex02/mutantstack.hpp
@@ -6,20 +6,46 @@
 /*   By: obouykou <obouykou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 19:03:05 by obouykou          #+#    #+#             */
-/*   Updated: 2021/03/30 19:05:59 by obouykou         ###   ########.fr       */
+/*   Updated: 2021/03/31 13:47:19 by obouykou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANT_STACK_H
 #define MUTANT_STACK_H
 
-class MutantStack
+#include <stack>
+
+template<typename T>
+class MutantStack : public std::stack<T>
 {
-private:
-	/* data */
-public:
-	MutantStack(/* args */);
-	~MutantStack();
+	private:
+		
+	public:
+		MutantStack<T>() : std::stack<T>() {}
+		MutantStack<T>(MutantStack const &src) : std::stack<T>(src){}
+		virtual ~MutantStack<T>(){}
+	
+    typedef typename std::stack<T>::container_type::iterator					iterator;
+    typedef typename std::stack<T>::container_type::const_iterator			const_iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator			reverse_iterator;
+    typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
+	
+	iterator	begin()			{ return std::stack<T>::c.begin(); }
+	
+	iterator	end()			{ return std::stack<T>::c.end(); }
+	
+	const_iterator cbegin()		{ return ( std::stack<T>::c.cbegin() );}
+	
+	const_iterator cend()		{ return ( std::stack<T>::c.cend() ); }
+
+	reverse_iterator rbegin()	{ return ( std::stack<T>::c.rbegin() );}
+	
+	reverse_iterator rend()		{ return ( std::stack<T>::c.rend() );}
+
+	const_reverse_iterator crbegin()	{ return ( std::stack<T>::c.crbegin() );}
+	
+	const_reverse_iterator crend()		{ return ( std::stack<T>::c.crend() );}
+	
 };
 
 
