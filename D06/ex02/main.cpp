afb68a2178a6ae84047ca6655710dad2d49320a4
OMAR BOUYKOURNE <omarbpixel@gmail.com>
2021-04-19 13:49:47 +0000

    fix identify_from_reference() function

--- D06/ex02/main.cpp
+++ D06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: obouykou <obouykou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/24 12:31:12 by obouykou          #+#    #+#             */
-/*   Updated: 2021/03/27 17:15:59 by obouykou         ###   ########.fr       */
+/*   Updated: 2021/04/19 13:44:09 by obouykou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,21 @@
 
 class Base
 {
-	public:
-		virtual ~Base(){};	
+public:
+	virtual ~Base(){};
 };
 
-class A : public Base {};	
-class B : public Base {};
-class C : public Base {};
+class A : public Base
+{
+};
+class B : public Base
+{
+};
+class C : public Base
+{
+};
 
-Base	*generate(void)
+Base *generate(void)
 {
 	Base *base;
 	int r;
@@ -33,18 +39,18 @@ Base	*generate(void)
 	std::cout << "Generated class instance: ";
 	switch (r)
 	{
-		case 0:
-			std::cout << "A" << std::endl;
-			base = new A;
-			break;
-		case 1:
-			std::cout << "B" << std::endl;
-			base = new B;
-			break ;
-		default:
-			std::cout << "C" << std::endl;
-			base = new C;
-			break;
+	case 0:
+		std::cout << "A" << std::endl;
+		base = new A;
+		break;
+	case 1:
+		std::cout << "B" << std::endl;
+		base = new B;
+		break;
+	default:
+		std::cout << "C" << std::endl;
+		base = new C;
+		break;
 	}
 	std::cout << "----------------------------------------" << std::endl;
 	return base;
@@ -53,13 +59,13 @@ Base	*generate(void)
 void identify_from_pointer(Base *p)
 {
 	std::cout << "Identifiying class instance type from pointer: ";
-	
+
 	Base *tester = p;
-	
-	A *a = dynamic_cast<A*>(tester);
-	B *b = dynamic_cast<B*>(tester);
-	C *c = dynamic_cast<C*>(tester);
-	
+
+	A *a = dynamic_cast<A *>(tester);
+	B *b = dynamic_cast<B *>(tester);
+	C *c = dynamic_cast<C *>(tester);
+
 	if (a != NULL)
 		std::cout << "A" << std::endl;
 	else if (b != NULL)
@@ -71,19 +77,34 @@ void identify_from_pointer(Base *p)
 void identify_from_reference(Base &p)
 {
 	std::cout << "Identifiying class instance type from reference: ";
-	
-	Base *tester = &p;
-	
-	A *a = dynamic_cast<A*>(tester);
-	B *b = dynamic_cast<B*>(tester);
-	C *c = dynamic_cast<C*>(tester);
 
-	if (a != NULL)
+	try
+	{
+		A &a = dynamic_cast<A &>(p);
+		(void)a;
 		std::cout << "A" << std::endl;
-	else if (b != NULL)
+	}
+	catch (std::bad_cast &bc)
+	{
+	}
+	try
+	{
+		B &b = dynamic_cast<B &>(p);
+		(void)b;
 		std::cout << "B" << std::endl;
-	else if (c != NULL)
+	}
+	catch (std::bad_cast &bc)
+	{
+	}
+	try
+	{
+		C &c = dynamic_cast<C &>(p);
+		(void)c;
 		std::cout << "C" << std::endl;
+	}
+	catch (std::bad_cast &bc)
+	{
+	}
 }
 
 int main(void)
